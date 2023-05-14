ce7d63c2507a15cb770b661e0ce2ba895c5f053d
evelyn <evelyncc132002@gmail.com>
2023-05-14 23:22:21 +0100

    feature: update and partial_update

--- phone_case/views.py
+++ phone_case/views.py
@@ -36,3 +36,14 @@ class PhoneCaseViewSet(GenericViewSet):
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
