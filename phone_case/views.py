c150d85c27c61e2afca800e5e817cbe3c1dd0fa0
evelyn <evelyncc132002@gmail.com>
2023-05-15 23:55:05 +0100

    feature: destroy

--- phone_case/views.py
+++ phone_case/views.py
@@ -47,3 +47,8 @@ class PhoneCaseViewSet(GenericViewSet):
     def partial_update(self, request, *args, **kwargs):
         kwargs['partial'] = True
         return self.update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
