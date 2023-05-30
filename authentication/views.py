96afac64e3191e424e81c223b4c53635c96de3b5
evelyn <evelyncc132002@gmail.com>
2023-05-30 16:51:41 +0100

    feature: solucion_problemas

--- authentication/views.py
+++ authentication/views.py
@@ -14,6 +14,13 @@ class RegisterViewSet(mixins.CreateModelMixin, GenericViewSet):
     permission_classes = [AllowAny]
     serializer_class = RegistrationSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
                   mixins.UpdateModelMixin, mixins.DestroyModelMixin, GenericViewSet):
