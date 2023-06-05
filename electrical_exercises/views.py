730634b2ada455fdb272d737878e3745f323a06f
evelyn <evelyncc132002@gmail.com>
2023-06-05 22:21:55 +0100

    feature: rest_frameworks.filters (SearchFilter and OrderingFilter)

--- electrical_exercises/views.py
+++ electrical_exercises/views.py
@@ -1,8 +1,8 @@
 # Django and DRF imports
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
-
 
 # waning_moon_design imports
 from electrical_exercises.models import Television, Fridge
@@ -13,9 +13,15 @@ class TelevisionViewSet(ModelViewSet):
     queryset = Television.objects.all()
     serializer_class = TelevisionSerializer
     lookup_field = 'serial_number'
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['inches', "serial_number"]
+    ordering_fields = "__all__"
 
 
 class FridgeViewSet(CreateModelMixin, UpdateModelMixin, ListModelMixin, GenericViewSet):
     queryset = Fridge.objects.all()
     serializer_class = FridgeSerializer
     lookup_field = 'id'
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['color']
+    ordering_fields = ['color']
