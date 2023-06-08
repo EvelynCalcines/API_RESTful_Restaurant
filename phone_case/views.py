1af0912e07d6d7e869791b8849cc1101e5e29297
evelyn <evelyncc132002@gmail.com>
2023-06-08 23:45:50 +0100

    feature: django_filters Phone Case

--- phone_case/views.py
+++ phone_case/views.py
@@ -1,6 +1,7 @@
 # Django and DRF imports
+import django_filters
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
-
 
 # waning_moon_design imports
 from .serializers import UpdatePhoneCaseSerializer, ListPhoneCaseSerializer, CreatePhoneCaseSerializer
@@ -11,6 +12,13 @@ class PhoneCaseViewSet(ModelViewSet):
     queryset = PhoneCase.objects.all()
     serializer_class = ListPhoneCaseSerializer
     lookup_field = 'id'
+    filter_backends = [SearchFilter, OrderingFilter, django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = {
+        "color": ["icontains", "isnull", "exact", "in"],
+        "stock": ["lt", "lte", "exact", "gte", "gt", "in"],
+    }
+    search_fields = ['name']
+    ordering_fields = ['brand']
 
     def get_serializer_class(self):
         if self.action in ["partial_update", "update"]:
