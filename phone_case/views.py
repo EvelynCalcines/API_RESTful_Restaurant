691d2ef54f3baa66ea0fd142da6e148a918c5b9c
evelyn <evelyncc132002@gmail.com>
2023-06-19 22:10:42 +0100

    feature: better-code in view

--- phone_case/views.py
+++ phone_case/views.py
@@ -1,7 +1,6 @@
 # Django and DRF imports
 import django_filters
 from rest_framework import status, mixins
-from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -18,7 +17,6 @@ class PhoneCaseViewSet(mixins.CreateModelMixin,
                        mixins.RetrieveModelMixin,
                        mixins.ListModelMixin,
                        GenericViewSet):
-
     queryset = PhoneCase.objects.all()
     serializer_class = ListPhoneCaseSerializer
     lookup_field = 'id'
@@ -30,12 +28,6 @@ class PhoneCaseViewSet(mixins.CreateModelMixin,
     search_fields = ['name']
     ordering_fields = ['brand']
 
-    def get_queryset(self):
-        queryset = self.queryset
-        if self.action in ("me_list", "me"):
-            queryset = queryset.filter(user=self.request.user.id)
-        return queryset
-
     def create(self, request, *args, **kwargs):
         request.data['user'] = request.user.id
         serializer = self.get_serializer(data=request.data)
@@ -44,40 +36,26 @@ class PhoneCaseViewSet(mixins.CreateModelMixin,
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    @action(detail=True, methods=['PUT', 'PATCH', 'DELETE'])
-    def me(self, request, id):
-
-        if request.method in ('PUT', 'PATCH'):
-            instance = self.get_object()
-            serializer = self.get_serializer(instance, data=request.data, partial=True)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
-
-        elif request.method == 'DELETE':
-            instance = self.get_object()
-            instance.delete()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(detail=False, methods=['GET'], url_path="me-list", url_name="me-list")
-    def me_list(self, request):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
     def get_serializer_class(self):
         if self.action == "create":
             return CreatePhoneCaseSerializer
-        elif self.action == 'me':
-            return UpdatePhoneCaseSerializer
-        elif self.action == 'me_list':
-            return ListProfilePhoneCaseSerializer
+        return self.serializer_class
 
+
+class MePhoneCaseView(mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      mixins.ListModelMixin,
+                      GenericViewSet):
+
+    queryset = PhoneCase.objects.all()
+    serializer_class = ListProfilePhoneCaseSerializer
+    lookup_field = "id"
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user.id)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in ("update", "partial_update"):
+            return UpdatePhoneCaseSerializer
         return self.serializer_class
